4e97af879b9db85ca96f8dbc1964fa81e15a6eea
Ibrar-ghani <ibrar.ghani.aaur@gmailcom>
2024-01-03 13:32:30 +0500

    Initial Commit

---
+++ softo-team/softo_teamApp.swift
@@ -0,0 +1,17 @@
+//
+//  softo_teamApp.swift
+//  softo-team
+//
+//  Created by user on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct softo_teamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
